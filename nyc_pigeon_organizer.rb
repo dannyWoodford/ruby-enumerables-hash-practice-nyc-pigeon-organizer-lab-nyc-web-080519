5c5ec5b122ad4f5ba05450e809d671c5e5877a24
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-31 00:27:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,10 @@ def nyc_pigeon_organizer(data)
   new_pigeon_org = {"Theo", "Peter Jr.", "Lucky"}
   
   data.each do |key, value|
+    value.each do |key,value|
     
   binding.pry
+    end
   end
   new_pigeon_org
 end
